9a9bad6eb10f9f267f21fdd577f6b40a1b7253a2
SomepalliBhargavi <somepallibhargavi245@gmail.com>
2025-06-16 15:58:22 +0530

    Add files via upload

---
+++ Python CRT/nested function.py
@@ -0,0 +1,15 @@
+def Eat():
+    def Washhands():
+        print("Wash hands...!")
+    def ServeFood():
+        print("Serve Food..!")
+    Washhands()
+    ServeFood()
+    print("Enjoy Your Meal...!")
+    Washhands()
+Eat()
+
+#print
+def Hello():
+    print("Hello ")
+print(Hello())#print(print("Hello"))
